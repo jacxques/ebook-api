13dc7e28d1f8267388de358cb7d2ae4d56f2887a
MeQHunter <fahmiadicandra9@gmail.com>
2022-10-25 08:02:49 +0700

    CRUD siswa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('hy there',function(){
+    return ["angger" => "Cantik"];
 });
+
+Route::resource('halocontroller', HelloController::class);
+Route::resource('siswa', SiswaController::class);
+

1aa07d8e5e93a56386e0c0493bfe32744b3fd853
MeQHunter <fahmiadicandra9@gmail.com>
2022-11-08 09:38:43 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HeloController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,4 +27,28 @@ Route::get('hy there',function(){
 
 Route::resource('halocontroller', HelloController::class);
 Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [AuthorController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
+
+
+// protected routes
+route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
+
+
 
